1dd56d067acd5e33e7b5132b727770153893005b
doyoung Hwang <dodo45133@gmail.com>
2020-05-09 22:27:23 +0900

    ga 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,8 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@plugins/vuetify'
+    '@plugins/vuetify',
+    '@plugins/analytics'
   ],
   /*
   ** Nuxt.js dev-modules
