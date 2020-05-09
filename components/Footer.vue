ddf798a968fbcc44828b783cd6323a2b548887f9
doyoung Hwang <dodo45133@gmail.com>
2020-05-09 22:13:02 +0900

    color 수정

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,6 +3,6 @@
     class="pa-3"
   >
     <v-spacer></v-spacer>
-    <div>&copy; {{ new Date().getFullYear() }}. CheeCheeStudio. All rights reserved. </div>
+    <div style="color:gray;">&copy; {{ new Date().getFullYear() }}. CheeCheeStudio. All rights reserved. </div>
   </v-footer>
 </template>
