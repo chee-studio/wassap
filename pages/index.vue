7a14556bf43aac6665cafa5e8cc269f25b390a1a
doyoung Hwang <dodo45133@gmail.com>
2020-05-09 21:37:40 +0900

    url 수정

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
       <v-card-text>
         <a
           class="grey--text text--lighten-3"
-          href="https://cafe.naver.com/winerack24"
+          href="https://cafe.naver.com/winerack24/28877"
           target="_blank"
         >★와쌉★ 와인 싸게 사는 사람들.xlsx</a>의 "내가 산 와인 가격 공유" 엑셀 데이터 기반입니다.
       </v-card-text>
