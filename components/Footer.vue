25c5beb3e66b34c24b36692653fa900edf435f22
doyoung Hwang <dodo45133@gmail.com>
2020-05-10 12:28:51 +0900

    footer 수정

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,6 @@
 <template>
-  <v-footer
-    class="pa-3"
-  >
+  <v-footer class="pa-3">
     <v-spacer></v-spacer>
-    <div style="color:gray;">&copy; {{ new Date().getFullYear() }}. CheeCheeStudio. All rights reserved. </div>
+    <div style="color:gray; font-size: 0.7rem;">&copy; {{ new Date().getFullYear() }}. CheeCheeStudio. All rights reserved. </div>
   </v-footer>
 </template>
