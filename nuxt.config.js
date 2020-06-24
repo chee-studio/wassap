c4d200c85fd6ffe4535df2e1fbfb5e6750898418
doyoung Hwang <dodo45133@gmail.com>
2020-06-24 22:47:59 +0900

    og.수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,8 +7,8 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name + 'ㅇㅇㅇ',
-    title: process.env.npm_package_name + 'ㅃㅂ' || '',
+    titleTemplate: 'Wassap',
+    title: 'Wassap',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
