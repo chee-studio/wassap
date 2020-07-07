740fb8eea5cd4362b7d54018531a92ac0d873b44
doyoung Hwang <dodo45133@gmail.com>
2020-07-07 21:29:19 +0900

    test

--- pages/test.vue
+++ pages/test.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
-    isMobile: {{$device.isMobile}}
+    isMobile: {{$device.isMobile}} <br/>
+    isDesktop: {{$device.isDesktop}} <br/>
+
+    {{$device.isDesktop ? 'https://cafe.naver.com/ArticleList.nhn?search.clubid=20564405&search.menuid=137&search.boardtype=L#' : 'https://m.cafe.naver.com/ca-fe/web/cafes/20564405/menus/137'}}
   </div>
 </template>
